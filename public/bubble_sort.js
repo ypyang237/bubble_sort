543901cb0c9d32575bdf7f1ce68ca244c3e3608c
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-29 14:53:36 -1000

    finishes extra challenge, sets it to array prototype method

--- public/bubble_sort.js
+++ public/bubble_sort.js
@@ -1,4 +1,40 @@
-function bubbleFunc(arr) {
+// function bubbleFunc(arr) {
+//   var isSorted = false;
+//   var count = 0;
+
+// while(!isSorted) {
+
+// isSorted = true;
+
+//   for (i = 0; i < arr.length; i++) {
+
+//     if (arr[i] > arr[i+1]) {
+//       count++;
+//       swap(arr, i);
+//       isSorted = false;
+//     }
+//   }
+
+//   }
+//   console.log(count);
+//   return arr;
+// }
+
+//   function swap(arr, i) {
+//     temp = arr[i];
+//     arr[i] = arr[i+1];
+//     arr[i+1] = temp;
+//   }
+
+// module.exports = bubbleFunc;
+
+//THIS TOP PART IS THE ORIGINAL CHALLENGE.
+//THE BOTTOM PART IS FOR THE CHALLENGE :
+//Create a way for all Arrays to be able to use your bubble sort function as a method of the Array object.
+//HAD TO CHANGE SPEC FILE WITH THIS
+
+function bubbleSort() {
+
   var isSorted = false;
   var count = 0;
 
@@ -6,19 +42,18 @@ while(!isSorted) {
 
 isSorted = true;
 
-  for (i = 0; i < arr.length; i++) {
+  for (i = 0; i < this.length; i++) {
 
-    if (arr[i] > arr[i+1]) {
+    if (this[i] > this[i+1]) {
       count++;
-      swap(arr, i);
+      swap(this, i);
       isSorted = false;
     }
   }
 
   }
   console.log(count);
-  return arr;
-}
+  return this;
 
   function swap(arr, i) {
     temp = arr[i];
@@ -26,7 +61,6 @@ isSorted = true;
     arr[i+1] = temp;
   }
 
+}
 
-
-
-module.exports = bubbleFunc;
+module.exports = bubbleSort;
--- test/bubble_sort.spec.js
+++ test/bubble_sort.spec.js
@@ -1,21 +1,37 @@
 var chai = require('chai');
-var bubbleFunction = require('../public/bubble_sort.js');
+// var bubbleFunction = require('../public/bubble_sort.js');
 var expect = chai.expect;
 var should = chai.should();
+var bubbleSort = require('../public/bubble_sort.js');
+Array.prototype.bubbleSort = bubbleSort;
 
-describe('bubble_sort function', function() {
+describe('bubbleFunc function', function() {
 
   it('should be a function', function(){
-    expect(bubbleFunction).to.be.a('function');
+    expect(bubbleSort).to.be.a('function');
   });
 
   it('should swap two numbers if the previous is larger than the current', function() {
     var myArray = [5, 3, 6];
-    bubbleFunction(myArray);
+    myArray.bubbleSort();
     expect(myArray).to.deep.equal([3, 5, 6]);
   });
 
     var nextArray = [1, 0, 75, 34, 2, 5, 7, 25, 56, 78, 12];
-    bubbleFunction(nextArray);
+    nextArray.bubbleSort();
     expect(nextArray).to.deep.equal([ 0, 1, 2, 5, 7, 12, 25, 34, 56, 75, 78 ]);
+});
+
+describe('bubbleSort function', function() {
+  it('should be a function', function() {
+    expect(Array.prototype.bubbleSort).to.be.a('function');
+  });
+
+  it('should sort numbers from low to high', function() {
+
+    var messyArr = [2,5,1,9,4];
+
+    expect(messyArr.bubbleSort()).to.deep.equal([1,2,4,5,9]);
+
+  });
 });
